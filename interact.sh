2d34a072e2574763567c1dfdd1ebbda7acc4b6ff
yuya sato <mrfool38@gmail.com>
2017-01-01 13:04:27 +0900

    refactor

--- interact.sh
+++ interact.sh
@@ -11,50 +11,55 @@ function cmdline() {
 }
 
 while IFS= read -r -n1 -s char; do
-  if [[ $char == $'\x1b' ]]; then
+  if [[ $char == $'\x1b' ]]; then # arrow key.
     read -r -n2 -s rest
     char+="$rest"
-  # elif [ "$char" = "" ] ; then
-  #   char="enter"
-  # elif [ "$char" = " " ] ; then
-  #   char="space"
-  # elif [ "$char" = " " ] ; then
-  #   char="space"
+  elif [ "$char" = "" ] ; then # enter. blank
+    char="enter"
+  elif [ "$char" = "" ] ; then # backspace. zero with non joiner
+    char="backspace"
+  elif [ "$char" = " " ] ; then # space.
+    char="space"
   fi
+
   case $char in
+
     $'\x1b\x5b\x41' ) # ↑  xxd
       # 一つ上のhistoryを表示
       count=`expr $count + 1`
       cmdline `echo -ne $history | tail -n $count | head -n1` ;;
+
     $'\x1b\x5b\x42' ) # ↓
       if [ $count -gt 0 ] ; then
         count=`expr $count - 1`
         cmdline
       fi
       cmdline `echo -ne $history | tail -n $count | head -n1` ;;
+
     $'\x1b\x5b\x43' ) ;; # →
+
     $'\x1b\x5b\x44' ) ;; # ←
+
     [0-9] )
       echo "cmd"$char &&
       # echo -e "history: "$history &&
       count=0
       history=$history"cmd"$char"\n" ;;
+
     c )
       cmdline ;;
 
-    * )
-      if [ "$char" = "" ] ; then # enter. blank
-        echo "enter"
-      fi
-      if [ "$char" = "" ] ; then # backspace. zero with non joiner
-        echo "backspace"
-      fi
-      if [ "$char" = " " ] ; then # space.
-        echo "backspace"
-        echo -n " "
-      fi
+    enter )
+      echo "enter" ;;
 
-      echo ${#char}
+    backspace )
+      echo "backspace" ;;
+
+    space )
+      echo "space"
+      echo -n " " ;;
+
+    * )
       echo -n $char ;;
   esac
 done
