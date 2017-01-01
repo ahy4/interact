bcb8a8390274f079cff723df9b528fb436cb4338
yuya sato <mrfool38@gmail.com>
2017-01-01 12:28:23 +0900

    enter, backspaceの認識

--- interact.sh
+++ interact.sh
@@ -1,34 +1,65 @@
 #!/bin/bash
 
 cmd=$1
-count=-1
+count=0
 history=""
+
+function cmdline() {
+  printf '\r%s' "                                    "
+  printf '\r%s' ""
+  printf '\r%s' $1
+}
+
 while IFS= read -r -n1 -s char; do
   if [[ $char == $'\x1b' ]]; then
     read -r -n2 -s rest
     char+="$rest"
+  # elif [ "$char" = "" ] ; then
+  #   char="enter"
+  # elif [ "$char" = " " ] ; then
+  #   char="space"
+  # elif [ "$char" = " " ] ; then
+  #   char="space"
   fi
   case $char in
-    $'\x1b\x5b\x41' ) # ↑
+    $'\x1b\x5b\x41' ) # ↑  xxd
       # 一つ上のhistoryを表示
       count=`expr $count + 1`
-      echo -e $history | tail -n $count | head -n1 ;;
+      cmdline `echo -ne $history | tail -n $count | head -n1` ;;
     $'\x1b\x5b\x42' ) # ↓
-      count=`expr $count - 1`
-      echo -e $history | tail -n $count | head -n1 ;;
-    $'\x1b\x5b\x43' ) # →
-      echo → ;;
-    $'\x1b\x5b\x44' ) # ←
-      echo ← ;;
+      if [ $count -gt 0 ] ; then
+        count=`expr $count - 1`
+        cmdline
+      fi
+      cmdline `echo -ne $history | tail -n $count | head -n1` ;;
+    $'\x1b\x5b\x43' ) ;; # →
+    $'\x1b\x5b\x44' ) ;; # ←
     [0-9] )
       echo "cmd"$char &&
-      echo -e "history: "$history &&
+      # echo -e "history: "$history &&
+      count=0
       history=$history"cmd"$char"\n" ;;
+    c )
+      cmdline ;;
 
     * )
+      if [ "$char" = "" ] ; then # enter. blank
+        echo "enter"
+      fi
+      if [ "$char" = "" ] ; then # backspace. zero with non joiner
+        echo "backspace"
+      fi
+      if [ "$char" = " " ] ; then # space.
+        echo "backspace"
+        echo -n " "
+      fi
+
+      echo ${#char}
       echo -n $char ;;
   esac
 done
+
+
 
 # read args
 # while test "$args" != "a"
