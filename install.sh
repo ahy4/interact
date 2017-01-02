3eb8e423ef63b4d2ae8d3058fbfd7171c3db71af
ahyahya <mrfool38@gmail.com>
2017-01-03 00:23:48 +0900

    fix: argument bug

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 #####
 # 以下を実行してください。
 # ただしmacだけ対応してます
-# curl -sL https://raw.githubusercontent.com/ahyahya/interact/master/install.sh | bash
+# curl -sL https://raw.githubusercontent.com/ahyahya/interact/develop/install.sh | bash
 #####
 
 cd /usr/local/bin
 
-curl -o _interact https://raw.githubusercontent.com/ahyahya/interact/master/_interact
-curl -o interact https://raw.githubusercontent.com/ahyahya/interact/master/interact
+curl -o _interact https://raw.githubusercontent.com/ahyahya/interact/develop/_interact
+curl -o interact https://raw.githubusercontent.com/ahyahya/interact/develop/interact
 
 chmod +x _interact
 chmod +x interact
